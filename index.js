ddf2274c893c6dd508c664be39e52fae670f3acb
Mehedi Hassan <meh67719@gamil.com>
2026-01-20 11:32:14 +0600

    index.js file update

--- index.js
+++ index.js
@@ -160,9 +160,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
 
 
 module.exports = app
